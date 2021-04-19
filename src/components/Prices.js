8dee4fea4a893ada92f305b2f0ea993a3fc95854
AZQIARA <damardika1998@gmail.com>
2021-04-19 15:41:55 +0700

    first

---
+++ src/components/Prices.js
@@ -0,0 +1,103 @@
+import React from "react";
+
+const Prices = () => {
+  return (
+    <div className="prices">
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <div className="price">
+              <div className="price__img">
+                <img src="/img/baju1.jpeg" alt="baju" />
+              </div>
+              <h1 className="price__heading">Komo Ring Cardigan Knit</h1>
+              <p className="price__text">
+                Nullam nibh sem, imperdiet ultrices commodo a, vulputate vel
+                ligula. Duis venenatis at eros sed egestas. Mauris rutrum quam
+                risus, vel hendrerit dui tempor in.
+              </p>
+              <p className="price_rs">52.000</p>
+            </div>
+          </div>
+
+          <div className="col-4">
+            <div className="price">
+              <div className="price__img">
+                <img src="/img/baju2.jpeg" alt="baju" />
+              </div>
+              <h1 className="price__heading">Whitebone Turtleneck Knit</h1>
+              <p className="price__text">
+                Nullam nibh sem, imperdiet ultrices commodo a, vulputate vel
+                ligula. Duis venenatis at eros sed egestas. Mauris rutrum quam
+                risus, vel hendrerit dui tempor in.
+              </p>
+              <p className="price_rs">52.000</p>
+            </div>
+          </div>
+
+          <div className="col-4">
+            <div className="price">
+              <div className="price__img">
+                <img src="/img/baju3.jpeg" alt="baju" />
+              </div>
+              <h1 className="price__heading">Tosca Motive Knivest</h1>
+              <p className="price__text">
+                Nullam nibh sem, imperdiet ultrices commodo a, vulputate vel
+                ligula. Duis venenatis at eros sed egestas. Mauris rutrum quam
+                risus, vel hendrerit dui tempor in.
+              </p>
+              <p className="price_rs">52.000</p>
+            </div>
+          </div>
+
+          <div className="col-4">
+            <div className="price">
+              <div className="price__img">
+                <img src="/img/baju4.jpeg" alt="baju" />
+              </div>
+              <h1 className="price__heading">Coat Fur Oversized Sweater Knit</h1>
+              <p className="price__text">
+                Nullam nibh sem, imperdiet ultrices commodo a, vulputate vel
+                ligula. Duis venenatis at eros sed egestas. Mauris rutrum quam
+                risus, vel hendrerit dui tempor in.
+              </p>
+              <p className="price_rs">55.000</p>
+            </div>
+          </div>
+
+          <div className="col-4">
+            <div className="price">
+              <div className="price__img">
+                <img src="/img/baju5.jpeg" alt="baju" />
+              </div>
+              <h1 className="price__heading">Top Zipper</h1>
+              <p className="price__text">
+                Nullam nibh sem, imperdiet ultrices commodo a, vulputate vel
+                ligula. Duis venenatis at eros sed egestas. Mauris rutrum quam
+                risus, vel hendrerit dui tempor in.
+              </p>
+              <p className="price_rs">52.000</p>
+            </div>
+          </div>
+
+          <div className="col-4">
+            <div className="price">
+              <div className="price__img">
+                <img src="/img/baju6.jpeg" alt="baju" />
+              </div>
+              <h1 className="price__heading">White Brocade Zipper Cardigan</h1>
+              <p className="price__text">
+                Nullam nibh sem, imperdiet ultrices commodo a, vulputate vel
+                ligula. Duis venenatis at eros sed egestas. Mauris rutrum quam
+                risus, vel hendrerit dui tempor in.
+              </p>
+              <p className="price_rs">49.000</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Prices;
